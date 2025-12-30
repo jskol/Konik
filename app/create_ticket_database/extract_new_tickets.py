876db3c9a02448f423d1b13164b8be6c3812ea28
jskol <skolimowski.jan@gmail.com>
2025-12-30 15:08:21 +0100

    new_DB + rerunning the tests for logic

---
+++ app/create_ticket_database/extract_new_tickets.py
@@ -0,0 +1,29 @@
+import os,sys
+cur_dir=os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(cur_dir))
+
+from app.create_ticket_database.data_base import TicketDataBase, compare_two_dicts_of_shows
+
+def  extract_new_tickets(DB_type: TicketDataBase,
+                         new_DB_file: str,
+                         ref_DB_file: str
+                         )->dict[tuple[str,str],dict[str,int]]:
+    
+    '''
+    Function that extracts only the events
+    that have more tickets then it had
+    in the ref_DB
+    '''
+    if not os.path.isfile(f'{new_DB_file}.{DB_type._extension}'):
+        raise FileNotFoundError
+    else:
+        read_DB = DB_type.importDB(new_DB_file)
+        try:
+            read_DB2 = DB_type.importDB(os.path.join(cur_dir,))
+            comp_Res=compare_two_dicts_of_shows(read_DB,read_DB2)    
+        except FileNotFoundError:
+            comp_Res=read_DB
+            
+        new_tickets={ k:v  for k,v in comp_Res.items() if any(v.values())}
+
+        return new_tickets
--- tests/test_update.py
+++ tests/test_update.py
@@ -22,6 +22,19 @@ def test_check_for_new_tickets(ref_DB, has_tickets):
     print(list(new_tickets.keys()))
     assert bool(len(new_tickets)>0) == has_tickets
 
+from app.create_ticket_database.extract_new_tickets import extract_new_tickets
+
+@pytest.mark.parametrize("ref_DB,has_tickets",test_cases)
+def test_compare_check_for_new_tickets_with_extract(ref_DB, has_tickets):
+    read_DB = DB.importDB(main_DB_path)
+    read_DB2 = DB.importDB(ref_DB)
+    comp_Res=compare_two_dicts_of_shows(read_DB,read_DB2)
+    new_tickets={ k:v  for k,v in comp_Res.items() if any(v.values())}
+
+    new_tickets_V2=extract_new_tickets(DB,main_DB_path,ref_DB)
+
+    for old,new in zip(new_tickets.items(),new_tickets_V2.items()):
+        assert bool(old[0]==new[0])== has_tickets
 
 
 if __name__=="__main__":

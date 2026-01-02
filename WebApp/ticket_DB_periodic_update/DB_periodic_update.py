05df58220c24b44aa9996a930ef7df8b8b340d6e
jskol <skolimowski.jan@gmail.com>
2026-01-02 20:37:13 +0100

    error cuaing the update to freeze due to not handling of the HTTPError now fixed--> at the end of check_link.py

--- WebApp/ticket_DB_periodic_update/DB_periodic_update.py
+++ WebApp/ticket_DB_periodic_update/DB_periodic_update.py
@@ -16,7 +16,6 @@ from app.main import event_type_list
 import datetime
 import datetime
 import asyncio,shutil
-from HF_download import upload_to_hf
 
 async def do_DB_update(
     DB_type: TicketDataBase,
@@ -31,54 +30,40 @@ async def do_DB_update(
     '''
     curr_dir=os.path.dirname(os.path.abspath(__file__))
     root_dir=os.path.dirname(os.path.dirname(curr_dir))
-    try:
-        while True:
-            curr_date=datetime.datetime.now()
-            date_str=curr_date.strftime("%d/%m/%Y %H:%M:%S")
-            print(f'Ruszam z aktualizacją {date_str}')
-            for event_it, event_type in enumerate(event_type_list):
-                DB_loc=os.path.join(root_dir,'Ticket_DB',f'DB_event_{event_it}')
-                # generate a dict of plays 12 months in advance
-                event_dict=gen_event_dict(months_in_advance,event_type,False)
-            
-                # Nice trick to run asynchronously
-                # run a function that returns None
-                # in this case in-place changes to
-                # an event dict
-                loop = asyncio.get_running_loop()
+    while True:
+        curr_date=datetime.datetime.now()
+        date_str=curr_date.strftime("%d/%m/%Y %H:%M:%S")
+        print(f'Ruszam z aktualizacją {date_str}')
+        for event_it, event_type in enumerate(event_type_list):
+            DB_loc=os.path.join(root_dir,'Ticket_DB',f'DB_event_{event_it}')
+            # generate a dict of plays 12 months in advance
+            event_dict=gen_event_dict(months_in_advance,event_type,False)
+           
+            # Nice trick to run asynchronously
+            # run a function that returns None
+            # in this case in-place changes to
+            # an event dict
+            loop = asyncio.get_running_loop()
+            await loop.run_in_executor(None,update_shows_dict,event_dict)
+             
+            final_dict=export_dict(event_dict)
+            #Make a legacy version
+            src=DB_loc+f'.{DB_type._extension}'
+            src_copy=DB_loc+f'_OLD.{DB_type._extension}'
+            if os.path.exists(src):
+                curr_date=datetime.datetime.now()
+                date_str=curr_date.strftime("%d/%m/%Y %H:%M:%S")
+                print(f"Robię kopię DB {src}->{src_copy} o {date_str}")
                 try:
-                    await loop.run_in_executor(None,update_shows_dict,event_dict)                  
-                    final_dict=export_dict(event_dict)
-                    #Make a legacy version
-                    src=DB_loc+f'.{DB_type._extension}'
-                    src_copy=DB_loc+f'_OLD.{DB_type._extension}'
-                    if os.path.exists(src):
-                        curr_date=datetime.datetime.now()
-                        date_str=curr_date.strftime("%d/%m/%Y %H:%M:%S")
-                        print(f"Robię kopię DB {src}->{src_copy} o {date_str}")
-                        try:
-                            shutil.copyfile(
-                                src,
-                                src_copy
-                            )
-                        except FileNotFoundError:
-                            print(f'Nie powiodło się tworzenie {src_copy}')
-                            
-                    # Do a two step swap of the DB
-                    DB_type.exportDB(final_dict,DB_loc)
-                    upload_to_hf_flag=bool(int(os.getenv("UPLOAD_TO_HF")))
-                    if upload_to_hf_flag:
-                        await loop.run_in_executor(None,upload_to_hf,src)
-
-                except Exception as e:
-                    print(f"Pojawił się problem {e} i nie będę aktualizował bazy danych") 
-
-                finally:
-                    gc.collect()
-            print("Biletowa baza danych jest aktualna")
-            for _ in range(wait_time//120):
-                await asyncio.sleep(120)
-    except asyncio.CancelledError:
-        print("DB update forced to stop.")
-        raise
-    
+                    shutil.copyfile(
+                        src,
+                        src_copy
+                    )
+                except FileNotFoundError:
+                    print(f'Nie powiodło się tworzenie {src_copy}')
+            
+            # Do a two step swap of the DB
+            DB_type.exportDB(final_dict,DB_loc)
+        print("Biletowa baza danych jest aktualna")
+        
+        await asyncio.sleep(wait_time)
